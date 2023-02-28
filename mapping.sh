499fa96680fb1dcce45add98ed306dc23ccd1e4f
rgalindor <robin.robeer@gmail.com>
2023-02-27 21:56:48 -0600

    Refactorizando mapeo

---
+++ mapping.sh
@@ -0,0 +1,19 @@
+for i in $(ls);
+do
+    echo $i
+done
+
+echo "Alineamientos y procesamiento"
+
+for i in  $(ls lecturas/*1P.fq)
+do
+    echo ${i/_1P.fq/}
+    hisat2 --dta -x $1 -1 $i -2 ${i/1P/2P} -S ${i/_1P.fq/.sam}
+    samtools view -bS -T ${1}.fa ${i/_1P.fq/.sam} > ${i/_1P.fq/.bam}
+    samtools sort ${i/_1P.fq/.bam} > ${i/_1P.fq/.sort.bam}
+    samtools index ${i/_1P.fq/.sort.bam}
+done
+
+
+
+
