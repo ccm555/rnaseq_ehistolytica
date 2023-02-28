749d6746810e69acb33e2697db745ed13468f621
rgalindor <robin.robeer@gmail.com>
2023-02-27 23:25:33 -0600

    changes to remove heavy data

--- mapping.sh
+++ mapping.sh
@@ -11,6 +11,7 @@ do
     hisat2 --dta -x $1 -1 $i -2 ${i/1P/2P} -S ${i/_1P.fq/.sam}
     samtools view -bS -T ${1}.fa ${i/_1P.fq/.sam} > ${i/_1P.fq/.bam}
     samtools sort ${i/_1P.fq/.bam} > ${i/_1P.fq/.sort.bam}
+    rm ${i/_1P.fq/.sam} ${i/_1P.fq/.bam}
     samtools index ${i/_1P.fq/.sort.bam}
 done
 
