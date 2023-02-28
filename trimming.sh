3abd2531ef6458ab532fb93604ab1706dd4157d1
rgalindor <robin.robeer@gmail.com>
2023-02-27 21:35:04 -0600

    Agregando trimming script

---
+++ trimming.sh
@@ -0,0 +1,7 @@
+for i in $(ls *R1_*.fastq.gz)
+do
+    trimmomatic PE \
+    ${i} $i{i/R1/R2} \
+    ${i/R1_001.fastq.gz/1P.fq} ${i/R1_001.fastq.gz/1U.fq} ${i/R1_001.fastq.gz/2P.fq} ${i/R1_001.fastq.gz/2U.fq} \
+    ILLUMINACLIP:${1}:2:30:10 SLIDINGWINDOW:5:25 MINLEN:50
+done
